647a25c51632e5d83844501a511d73478af0bc9b
julbouln <jboulnois@gmail.com>
2019-12-12 11:36:36 +0100

    1.2.2: utf8 firstname database + name countries

--- lib/im_helpers/name_extractor.rb
+++ lib/im_helpers/name_extractor.rb
@@ -9,42 +9,80 @@ module ImHelpers
 
   class FirstNameHash
 
-    def self.set(name, gender)
+    def self.set(name, gender, countries)
+      if name.to_ascii != name
+        set name.to_ascii, gender, countries
+      end
+
       if name.include? "+"
         ['', '-', ' '].each { |replacement|
-          set name.gsub("+", replacement), gender
+          set name.gsub("+", replacement), gender, countries
         }
       else
-        @names[name] ||= []
-        @names[name] << gender
+          @names[name] ||= []
+          @names[name] << {gender: gender, countries: countries}
       end
+    end
+
+    def self.countries
+      {"GB" => 0, "IE" => 1, "US" => 2, "IT" => 3, "MT" => 4, "PT" => 5, "ES" => 6,
+       "FR" => 7, "BE" => 8, "LU" => 9, "NL" => 10, "IGNORE0" => 11, "DE" => 12, "AT" => 13,
+       "CH" => 14, "IS" => 15, "DK" => 16, "NO" => 17, "SE" => 18, "FI" => 19, "EE" => 20,
+       "LV" => 21, "LT" => 22, "PL" => 23, "CZ" => 24, "SK" => 25, "HU" => 26, "RO" => 27,
+       "BG" => 28, "BA" => 29, "HR" => 30, "IGNORE1" => 31, "MK" => 32, "ME" => 33, "RS" => 34,
+       "SI" => 35, "AL" => 36, "GR" => 37, "RU" => 38, "BY" => 39, "MD" => 40, "UA" => 41,
+       "AM" => 42, "AZ" => 43, "GE" => 44, "KZ" => 45, "TR" => 46, "IR" => 47, "IL" => 48,
+       "CN" => 49, "IN" => 50, "JP" => 51, "KR" => 52, "VN" => 53, "OTHER" => 54
+      }
+    end
+
+    def self._rev_countries
+      h = {}
+      self.countries.each do |country, col|
+        h[col] = country
+      end
+      h
+    end
+
+    def self.rev_countries
+      @rev_countries ||= self._rev_countries
     end
 
     def self.parse_name_line(line)
       return if line.start_with?("#") or line.start_with?("=")
 
-      parts = line.split(" ").select { |p| p.strip != "" }
-      name = Unicode.downcase(parts[1])
+      genre_k = line[0..2].strip
+      name = Unicode.downcase(line[3..28].strip)
+      countries_freq = line[30..85].split('')
+      countries = []
+      countries_freq.each_with_index do |cf, i|
+        if cf.to_i > 0
+          countries << self.rev_countries[i]
+        end
+      end
 
-      case parts[0]
+      genre = nil
+      case genre_k
       when "M" then
-        set(name, :male)
+        genre = :male
       when "1M", "?M" then
-        set(name, :mostly_male)
+        genre = :mostly_male
       when "F" then
-        set(name, :female)
+        genre = :female
       when "1F", "?F" then
-        set(name, :mostly_female)
+        genre = :mostly_female
       when "?" then
-        set(name, :andy)
+        genre = :andy
       else
         raise "Not sure what to do with a gender of #{parts[0]}"
       end
+
+      set(name, genre, countries)
+
     end
 
     def self.init(fname)
       @names = PStore.new("/tmp/firstnames.pstore")
-      file = File.open(fname)
       @names.transaction do
         File.foreach(fname) do |line|
           parse_name_line line
@@ -75,10 +113,12 @@ module ImHelpers
   end
 
   class NameExtractor
+    attr_accessor :genres, :countries
     def initialize(name)
       @lastnames = []
       @firstnames = []
       @genres = []
+      @countries = []
       @orig_name = name
       parse
     end
@@ -124,11 +164,7 @@ module ImHelpers
     end
 
     def inspect
-      "#{to_s}<firstname:#{firstname_found? ? firstname : "?"},lastname:#{lastname_found? ? lastname : "?"},genres:#{firstname_found? ? genres : "?"}>"
-    end
-
-    def genres
-      @genres
+      "#{to_s}<firstname:#{firstname_found? ? firstname : "?"},lastname:#{lastname_found? ? lastname : "?"},genres:#{firstname_found? ? genres : "?"},countries:#{firstname_found? ? countries : "?"}>"
     end
 
 # 0 == exact match, 1 == completely different
@@ -172,7 +208,8 @@ module ImHelpers
         end
         if res or Unicode.downcase(name) =~ /^[a-z]\.?$/
           if res
-            @genres += res
+            @genres << res.first[:gender]
+            @countries += res.first[:countries]
           end
           @firstnames << name
         else
