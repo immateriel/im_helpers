06c4f62c4a4a97f91f0b67906ad6af401e00d7ec
Elodie Ailleaume <Didi@MacBook-Pro-de-Elodie.local>
2021-07-23 16:06:13 +0200

    Ajout du Liechtenstein, de la Moldavie, du Montenegro, et de la Serbie en Europe.

--- lib/im_helpers/territories.rb
+++ lib/im_helpers/territories.rb
@@ -80,7 +80,7 @@ module ImHelpers
     def self.europe
       ["AL", "AD", "AM", "AT", "BY", "BE", "BA", "BG", "CH", "CY", "CZ", "DE", "DK", "EE", "ES", "FO", "FI", "FR",
        "GB", "GE", "GI", "GR", "HU", "HR", "IE", "IS", "IT", "LT", "LU", "LV", "MC", "MK", "MT", "NO", "NL", "PL",
-       "PT", "RO", "RU", "SE", "SI", "SK", "SM", "TR", "UA", "VA"]
+       "PT", "RO", "RU", "SE", "SI", "SK", "SM", "TR", "UA", "VA", "LI", "MD", "ME", "RS"]
     end
 
     def self.francophonie
